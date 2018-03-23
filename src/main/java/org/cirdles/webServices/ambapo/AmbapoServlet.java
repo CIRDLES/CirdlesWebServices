8da916d074ef7971aa0909e8e4fc59692c5a601d
Tyler Newman <tnewman16@gmail.com>
2018-03-23 18:52:05 -0400

    Ambapo endpoint for UTM to LatLng conversion

--- src/main/java/org/cirdles/webServices/ambapo/AmbapoServlet.java
+++ src/main/java/org/cirdles/webServices/ambapo/AmbapoServlet.java
@@ -6,32 +6,22 @@
 package org.cirdles.webServices.ambapo;
 
 import java.io.IOException;
-import java.io.PrintWriter;
+import java.math.BigDecimal;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import org.springframework.web.bind.ServletRequestUtils;
+import org.cirdles.webServices.requestUtils.*;
+import org.cirdles.ambapo.*;
+import org.json.JSONObject;
 
 /**
  *
  * @author ty
  */
 public class AmbapoServlet extends HttpServlet {
-
-    /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
-        
-    }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
     /**
@@ -45,7 +35,7 @@ public class AmbapoServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
+        
     }
 
     /**
@@ -59,9 +49,46 @@ public class AmbapoServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
+        String uri = request.getRequestURI().toLowerCase();
+        String[] pieces = uri.split("/");
+        // first item will be "", second item will be "ambapo"
+        if (pieces.length >= 4) {
+            // UTM -> LatLng
+            if (pieces[2].equals("utm") && pieces[3].equals("latlng")) {
+                JSONObject json = RequestJSONUtils.extractRequestJSON(request);
+                BigDecimal easting = BigDecimal.valueOf(json.getLong("easting"));
+                BigDecimal northing = BigDecimal.valueOf(json.getLong("northing"));
+                String hemStr = json.getString("hemisphere");
+                int zoneNumber = json.getInt("zoneNumber");
+                String zoneStr = json.getString("zoneLetter");
+                String datum = json.getString("datum");
+                if (easting != null && northing != null &&
+                        hemStr != null && !hemStr.isEmpty() &&
+                        zoneStr != null && !zoneStr.isEmpty() &&
+                        datum != null && !datum.isEmpty()) {
+                    char hemisphere = hemStr.charAt(0);
+                    char zoneLetter = zoneStr.charAt(0);
+                    try {
+                        UTM utm = new UTM(easting, northing, hemisphere, zoneNumber, zoneLetter);
+                        Coordinate coord = UTMToLatLong.convert(utm, datum);
+                        JSONObject responseJson = new JSONObject();
+                        responseJson.put("latitude", coord.getLatitude());
+                        responseJson.put("longitude", coord.getLongitude());
+                        responseJson.put("datum", coord.getDatum());
+                        response.setContentType("application/json");
+                        response.getWriter().println(responseJson);
+                    } catch (Exception e) {
+                        // TODO: what to send as response?
+                        response.sendError(491);
+                    }
+                } else {
+                    // TODO: what to send as response?
+                    response.sendError(490);
+                }
+            }
+        }
     }
-
+    
     /**
      * Returns a short description of the servlet.
      *
@@ -69,7 +96,7 @@ public class AmbapoServlet extends HttpServlet {
      */
     @Override
     public String getServletInfo() {
-        return "Short description";
+        return "Ambapo Servlet";
     }// </editor-fold>
 
 }
