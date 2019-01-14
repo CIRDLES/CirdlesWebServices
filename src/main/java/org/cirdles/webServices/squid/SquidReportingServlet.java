d283309cc17c0e79f5f029c07c24da686e821c4b
Jim Bowring <bowring@gmail.com>
2019-01-14 06:58:51 -0500

    trying gdrive

--- src/main/java/org/cirdles/webServices/squid/SquidReportingServlet.java
+++ src/main/java/org/cirdles/webServices/squid/SquidReportingServlet.java
@@ -108,18 +108,21 @@ public class SquidReportingServlet extends HttpServlet {
                     fileName, fileStream, fileStream2, useSBM, useLinFits, refMatFilter, concRefMatFilter,
                     preferredIndexIsotopeName).toFile();
 
-            // this writes to catalinahome / bin
+//            // this writes to catalinahome / bin
+//            try {
+//                File localReportsZipFile = new File("LOCAL_ZIP.zip");
+//                Files.copy(report.toPath(), localReportsZipFile.toPath(), StandardCopyOption.REPLACE_EXISTING);
+//            } catch (IOException iOException) {
+//            }
+            // now if Linux. we are going to assume cirdles.cs.cofc.edu and write to Google Drive for now
             try {
-                File localReportsZipFile = new File("LOCAL_ZIP.zip");
-                Files.copy(report.toPath(), localReportsZipFile.toPath(), StandardCopyOption.REPLACE_EXISTING);
+                if (SystemUtils.IS_OS_LINUX) {
+                    String[] arguments = new String[]{
+                        "/home/gdrive", "upload", "--parent", "19RHlWggIw5fqWQUO1xs3M2iWjD82Ph3m", report.getPath()};
+                    Process proc = new ProcessBuilder(arguments).start();
+                }
             } catch (IOException iOException) {
             }
-//            // now if Linux. we are going to assume cirdles.cs.cofc.edu and write to Google Drive for now
-//            if (SystemUtils.IS_OS_LINUX) {
-//                String[] arguments = new String[]{
-//                    "/home/gdrive", "upload", "--parent", "19RHlWggIw5fqWQUO1xs3M2iWjD82Ph3m", "LOCAL_ZIP.zip"};
-//                Process proc = new ProcessBuilder(arguments).start();
-//            }
 
             response.setContentLengthLong(report.length());
             IOUtils.copy(new FileInputStream(report), response.getOutputStream());
