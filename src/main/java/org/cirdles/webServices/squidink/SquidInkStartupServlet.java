dba268916e5c258c547c3343fa00cc73bab54852
Josh Gilley <joshdgilley@gmail.com>
2020-11-08 16:30:23 -0500

    (UPDATE) Added squidink container clickable container functionality

--- src/main/java/org/cirdles/webServices/squidink/SquidInkStartupServlet.java
+++ src/main/java/org/cirdles/webServices/squidink/SquidInkStartupServlet.java
@@ -1,32 +1,27 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package org.cirdles.webServices.squidink;
-import java.sql.*;
 
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTCreationException;
 import org.apache.commons.io.IOUtils;
-import java.io.IOException;
-import java.math.BigDecimal;
-import java.util.Arrays;
+import org.cirdles.ambapo.*;
+import org.cirdles.webServices.requestUtils.JSONUtils;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.sql.RowSet;
+import java.io.File;
+import java.io.IOException;
+import java.math.BigDecimal;
+import java.sql.*;
+import java.util.Arrays;
+import java.util.Calendar;
+import java.util.Date;
 
-import org.cirdles.webServices.requestUtils.*;
-import org.cirdles.ambapo.*;
-import org.json.JSONException;
-import org.json.JSONObject;
-
-/**
- *
- * @author ty
- */
-public class SquidInkRegisterServlet extends HttpServlet {
-
+public class SquidInkStartupServlet extends HttpServlet {
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
     /**
      * Handles the HTTP <code>GET</code> method.
@@ -53,58 +48,21 @@ public class SquidInkRegisterServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        try {
-            //Initialize DB Connection and Statement loading
-            Class.forName("org.sqlite.JDBC");
-            Connection connection = DriverManager.getConnection("jdbc:sqlite:/Users/joshdgilley/Documents/College_of_Charleston/Fall_2020/Tutorial/cirdlesWebUI/CirdlesWebServices-master/users.db");
-            Statement statement = connection.createStatement();
-            statement.setQueryTimeout(30);
 
-            //Separate response data
-            String body = IOUtils.toString(request.getReader());
-            body = body.replace("\"","");
-            String[] array = body.split(":");
-            String holder = Arrays.toString(array);
-            String[] holder2 = holder.split(",");
-
-
-            //If email does not exist in table, push all values to table
-            PreparedStatement prep = connection.prepareStatement("select userID from Users where userID = ?");
-            prep.setString(1, holder2[1]);
-            ResultSet rs = prep.executeQuery();
-            if( rs.next() == false ) {
-                PreparedStatement input = connection.prepareStatement("insert into Users (userID, userName, password) values (?, ?, ?)");
-                input.setString(1, holder2[3]);
-                input.setString(2, holder2[1]);
-                input.setString(3, holder2[5]);
-                input.execute();
-                connection.close();
-                response.setContentType("application/json");
-                response.getWriter().println("Done");
-            }
-            else {
-                connection.close();
-                response.setStatus(400);
-                response.getWriter().println("Email Already Exists");
-            }
-            //json = JSONUtils.createResponseErrorJSON("Invalid URI");
-
+        try{
+            System.out.println(response);
+            response.getWriter().println("We hit this!");
+        } catch(IOException e){
+            e.printStackTrace();
         }
-        catch(SQLException | ClassNotFoundException e) {
-            System.err.println(e.getMessage());
-            response.getWriter().println(e.getMessage());
-        }
+
+        response.addHeader("Access-Control-Allow-Origin", "*");
+        Process process = Runtime.getRuntime()
+                .exec("docker run -p 81:8080 squidboys");
+
+
     }
 
-    private void createTable(Connection conn) throws SQLException {
-        String sqlCreate = "CREATE TABLE IF NOT EXISTS " + "Users"
-                + "  userID       VARCHAR(255),"
-                + "  userName     VARCHAR(255),"
-                + "  password     VARCHAR(255))";
-
-        Statement stmt = conn.createStatement();
-        stmt.execute(sqlCreate);
-    }
     private JSONObject handleUtmToLatlong(HttpServletRequest request,
                                           HttpServletResponse response) throws IOException {
         JSONObject json = JSONUtils.extractRequestJSON(request);
