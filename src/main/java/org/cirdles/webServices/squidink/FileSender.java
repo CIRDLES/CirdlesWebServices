9552f99276aec221b390a36668661caf4d5bc2c1
RichardMcCarty1 <58711057+RichardMcCarty1@users.noreply.github.com>
2020-11-29 13:09:43 -0500

    (ADDED) File Persistence on Login
    Sends files to container on user login

--- src/main/java/org/cirdles/webServices/squidink/FileSender.java
+++ src/main/java/org/cirdles/webServices/squidink/FileSender.java
@@ -8,18 +8,19 @@ package org.cirdles.webServices.squidink;
 import java.io.*;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import java.nio.file.Files;
+import java.net.URLConnection;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.*;
-import org.cirdles.squid.utilities.FileUtilities;
-import org.cirdles.squid.utilities.fileUtilities.ZipUtility;
+
+import org.apache.http.entity.mime.HttpMultipartMode;
+import org.apache.http.entity.mime.MultipartEntity;
+import org.apache.http.entity.mime.content.FileBody;
 import org.zeroturnaround.zip.*;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.nio.file.Files;
 
 
 /**
@@ -54,19 +55,48 @@ public class FileSender extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         try {
-        //On Post Request that carries userID, Take user id, find user ID folder, zip, send
+            //On Post Request that carries userID, Take user id, find user ID folder, zip, send
+            String user = request.getPathInfo();
+            ZipUtil.pack(new File("C:/Users/Richard McCarty/Downloads/CirdlesWeb/", user.replace("/", "")),
+                    new File("C:/Users/Richard McCarty/Downloads/CirdlesWeb/", user.replace("/", "") + ".zip"));
+            //Load file into FileInputStream
+            InputStream fio = new FileInputStream(new File("C:/Users/Richard McCarty/Downloads/CirdlesWeb/",
+                    user.replace("/", "") + ".zip"));
+            //Create Endpoint Connection
+            URLConnection connection = new URL("http://192.168.99.100:8081/squid_servlet/retrieve").openConnection();
+            //Casting to HTTPUrlConnection type for requestMethod
+            HttpURLConnection http = (HttpURLConnection) connection;
+            http.setRequestMethod("POST");
+            http.setDoOutput(true);
+            FileBody fileBody = new FileBody(new File("C:/Users/Richard McCarty/Downloads/CirdlesWeb/", user.replace("/", "") + ".zip"));
+            MultipartEntity multipartEntity = new MultipartEntity(HttpMultipartMode.STRICT);
+            multipartEntity.addPart("file", fileBody);
+            connection.setRequestProperty("Content-Type", multipartEntity.getContentType().getValue());
+            OutputStream out = connection.getOutputStream();
+            try {
+                multipartEntity.writeTo(out);
+            } finally {
+                out.close();
+            }
 
-        String user = request.getPathInfo();
-        ZipUtil.pack(new File("C:/Users/Richard McCarty/Downloads/CirdlesWeb/", user.replace("/","")),
-                new File("C:/Users/Richard McCarty/Downloads/CirdlesWeb/", user.replace("/","") + ".zip"));
-        System.out.println(user);
-        response.getWriter().println(user);
+            //Read Response
+            try (BufferedReader br = new BufferedReader(
+                    new InputStreamReader(http.getInputStream(), "utf-8"))) {
+                StringBuilder resp = new StringBuilder();
+                String responseLine = null;
+                while ((responseLine = br.readLine()) != null) {
+                    resp.append(responseLine.trim());
+                }
+                System.out.println(resp.toString());
+            }
+                http.disconnect();
+            }
+            catch (InvalidPathException | IOException e) {
+                System.out.println(e);
+                response.getWriter().println(e);
+            }
         }
-        catch (InvalidPathException | IOException e) {
-            System.out.println(e);
-            response.getWriter().println(e);
-        }
-    }
+
 
     /**
      * Returns a short description of the servlet.
