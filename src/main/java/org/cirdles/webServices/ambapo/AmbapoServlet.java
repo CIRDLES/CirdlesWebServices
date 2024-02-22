966bab85d843a5c5323033a46e09e732b24f6c2c
LongTangGithub <tanglh@g.cofc.edu>
2024-02-22 12:05:32 -0500

    Initial Commit: Jakarta

--- src/main/java/org/cirdles/webServices/ambapo/AmbapoServlet.java
+++ src/main/java/org/cirdles/webServices/ambapo/AmbapoServlet.java
@@ -7,10 +7,10 @@ package org.cirdles.webServices.ambapo;
 
 import java.io.IOException;
 import java.math.BigDecimal;
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import org.cirdles.webServices.requestUtils.*;
 import org.cirdles.ambapo.*;
 import org.json.JSONException;
